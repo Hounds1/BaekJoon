115a763335db7836a394f13240825ffee67cdbb6
Hounds1 <92633272+Hounds1@users.noreply.github.com>
2025-12-06 12:47:29 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1020 ms, Memory: 161800 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -9,20 +9,21 @@ public class Main {
 
         String discard = bufferedReader.readLine();
 
-        int[] cards = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Map<Integer, Integer> map = new HashMap<>();
+        String[] cards = bufferedReader.readLine().split(" ");
+        
+        Map<String, Integer> map = new HashMap<>();
 
-        for (int card : cards) {
+        for (String card : cards) {
             Integer val = map.computeIfAbsent(card, k -> 0);
             map.put(card, val + 1);
         }
 
         String discard2 = bufferedReader.readLine();
 
-        int[] targets = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        String[] targets = bufferedReader.readLine().split(" ");
 
         StringBuilder sb = new StringBuilder();
-        for (int target : targets) {
+        for (String target : targets) {
             Integer val = map.get(target);
 
             if (val == null) sb.append(0).append(" ");
